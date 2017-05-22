557f8f49b2f0dc086819f29983fe6c67143d678d
xyy55 <1037730272@qq.com>
2017-05-22 18:09:32 +0800

    Signed-off-by: xyy55 <1037730272@qq.com> 爬去豆瓣前250的电影，并在80s.tw网站获取对应的迅雷下载链接

---
+++ python3/top205movie.py
@@ -0,0 +1,117 @@
+#-*- encoding: utf-8 -*-
+from urllib.request import urlopen,Request
+from urllib.parse import urlencode
+from bs4 import BeautifulSoup
+from urllib.error import URLError,HTTPError
+from time import sleep
+
+def get_moive_name():
+    movie_name = []
+    for i in range(0,250,25):
+        url = 'https://movie.douban.com/top250?start='+str(i)+'&filter='
+        bs = BeautifulSoup(urlopen(url),'html.parser')
+        movies = bs.find('ol',{'class':'grid_view'})
+        for movie in movies.findAll('li'):
+            movie_name.append(movie.img.attrs['alt'])
+    return movie_name
+
+def search_movie(movie_name = []):
+    if len(movie_name) == 0:
+        return
+    url = 'http://www.80s.tw/search'
+    urls = []
+    for movie in movie_name:
+        print(movie)
+        post_data = {'keyword':movie}
+        post_data = urlencode(post_data)
+        post_data = post_data.encode(encoding='utf_8')
+        req = Request(url, post_data)
+        try:
+            bs = BeautifulSoup(urlopen(req),'html.parser')
+        except HTTPError:
+            sleep(3)
+            bs = BeautifulSoup(urlopen(req),'html.parser')
+        try:
+            movie_content = bs.find('ul',{'class':'clearfix'}).a
+        except Exception:
+            u = ''
+            urls.append(u)
+            continue
+        name = movie_content.get_text().strip()
+        if movie not in name:
+            u = ''
+        else:
+            u = movie_content.attrs['href']
+        if u != '':
+            u = 'http://www.80s.tw'+u+'/bd-1'
+        urls.append(u)
+    return urls
+
+def get_download_link(urls = [],movie_name = []):
+    if len(urls) == 0:
+        return
+    names = []
+    links = []
+    i = -1
+    for url in urls:
+        i += 1
+        if url == '':
+            print(movie_name[i]+'\t找不到该电影')
+            names.append(movie_name[i])
+            links.append('Null')
+            continue
+        
+        try:
+            bs = BeautifulSoup(urlopen(url),'html.parser')
+        except URLError:
+            url = url.replace('bd-1','hd-1')
+            try:
+                bs = BeautifulSoup(urlopen(url),'html.parser')
+            except Exception:
+                print(movie_name[i]+'\t没有标清以上资源')
+                names.append(movie_name[i])
+                links.append('Null')
+                continue
+        except Exception:
+            print(movie_name[i]+'\t反正就是打不开')
+            names.append(movie_name[i])
+            links.append('Null')
+            continue
+            
+        name = bs.findAll('a',{'rel':'nofollow'})[0].get_text().strip()
+        if name == '高清版':
+            name = movie_name[i]
+        link = bs.findAll('a',{'rel':'nofollow'})[1].attrs['href']
+        print(name,link)
+        names.append(name)
+        links.append(link)
+        
+    return names,links
+
+def main():
+    #movie_name = get_moive_name()
+    movie_name = []
+    file = open("E:\\movie_name.txt",'r')
+    for name in file:
+        movie_name.append(name.strip())
+    file.close()
+    '''urls = search_movie(movie_name)
+    file = open("E:\\urls.txt",'w')
+    for url in urls:
+        file.write(url+'\n')
+    file.close()'''
+    file = open("E:\\urls.txt",'r')
+    urls = []
+    for i in file:
+        urls.append(i.strip())
+    file.close()
+    names,links = get_download_link(urls,movie_name)
+    file = open("E:\\links.txt",'w')
+    for link in links:
+        file.write(link+'\n')
+    file.close()
+        
+if __name__ == '__main__':
+    main()
+    
+    
--- python3/wenjuanxing.py
+++ python3/wenjuanxing.py
@@ -8,10 +8,9 @@ from time import sleep
 url = 'https://sojump.com/handler/processjq.ashx?submittype=1&curID=13930071&t=1494382443451&starttime=2017%2F5%2F10%2010%3A13%3A53&rn=1779472485.29857501&sd=http%3a%2f%2fwww.sojump.com%2f'
 
 
-i = 1
+i = 0
 while True:
-    if i%3 == 0:
-        sleep(60)
+    #该处可以按照自己的需要生成随机数
     data = '1$1}2$2}3$2}4$2}5$'
     post_data = {'submitdata':data}
     post_data = urlencode(post_data)
@@ -20,7 +19,11 @@ while True:
     response = request.urlopen(url,post_data)
     #读取相应信息并解码
     html = response.read().decode("utf-8")
-    print(data,i)
+    print(data,i+1)
+    #成功的话会出现一串字符，末尾数字表示当前完成的总份数   
     print(html)
     i = i+1
+    if i%3 == 0:
+        #此处是为了防止同一IP地址提交太频繁需要输入验证码
+        sleep(60)
     
